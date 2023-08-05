56c648a8bd0f9574e4fb01430489548596fb2568
Tuan Au Duong <audtuan@gmail.com>
2023-08-05 16:41:11 +0700

    bt4 update

--- BT4/RandomStudents/Program.cs
+++ BT4/RandomStudents/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 // Tạo mới project với tên RandomStudents.
 
 // Phát sinh ngẫu nhiên 10 sinh viên và in thông tin ra màn hình.
@@ -17,22 +16,46 @@ class SinhVien
 		return $"{MSSV} - {HoTen} - {DiaChi}";
 	}
 }
+class RandomAddress
+{
+	private static string[] Streets = new string[] { "Cách Mạng Tháng Tám", "Nguyễn Thị Minh Khai" };
+	private static string[] Districts = new string[] { "Quận 1", "Quận Bình Thạnh", "Quận 3", "Quận Gò Vấp", "Quận 4", "Quận Phú Nhuận", "Quận 5", "Quận Tân Bình", "Quận 6", "Quận Tân Phú", "Quận 7", "Huyện Bình Chánh", "Quận 8", "Huyện Cần Giờ", "Quận 10", "Huyện Củ Chi", "Quận 11", "Huyện Hóc Môn", "Quận 12", "Huyện Nhà Bè" };
+}
+class RandomName
+{
+	private static string[] Surnames = new string[] { "Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Phan", "Vũ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương", "Lý" };
+	private static string[] MiddleNames = new string[] { "Hữu", "Gia", "Đình", "Hoài", "Huy", "Khánh", "Hải", "Bảo", "An", "Phúc", "Anh" };
+	private static string[] GivenNames = { "Trang", "Anh", "Linh", "Phương", "Hương", "Thảo", "Hà", "Huyền", "Ngọc", "Hằng", "Giang", "Nhung", "Yến", "Nga", "Mai", "Thu", "Hạnh", "Vân", "Hoa", "Hiền" };
 
-string[] HO = new string[] { "Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Phan", "Vũ", "Đặng", "Bùi", "Đỗ", "Hồ", "Ngô", "Dương", "Lý" };
-string[] DEM = new string[] { "Hữu", "Gia", "Đình", "Hoài", "Huy", "Khánh", "Hải", "Bảo", "An", "Phúc", "Anh" };
-string[] TEN = { "Trang", "Anh", "Linh", "Phương", "Hương", "Thảo", "Hà", "Huyền", "Ngọc", "Hằng", "Giang", "Nhung", "Yến", "Nga", "Mai", "Thu", "Hạnh", "Vân", "Hoa", "Hiền" };
+	private Random rnd;
+
+	public RandomName()
+	{
+		rnd = new Random();
+	}
+
+	public String Next()
+	{
+		string surname = Surnames[rnd.Next(0, Surnames.Length)];
+		string middleName = MiddleNames[rnd.Next(0, MiddleNames.Length)];
+		string givenName = GivenNames[rnd.Next(0, GivenNames.Length)];
+		return $"{surname} {middleName} {givenName}";
+	}
+
+}
 
 class RandomStudent
 {
 	private int _start = 1;
 	private char _name = 'A';
 
+
 	public SinhVien Next()
 	{
-		SinhVien result = new SinhVien()
+		SinhVien result = new SinhVien
 		{
 			MSSV = $"00{_start}",
-			HoTen = $"Nguyen Van {_name}",
+			HoTen = (new RandomName()).Next(),
 			DiaChi = $"Random ne",
 		};
 		_start++;
@@ -41,4 +64,11 @@ class RandomStudent
 	}
 }
 
-SinhVien result = new SinhVien()
+class Program
+{
+	public static void Main(string[] args)
+	{
+		RandomStudent rds = new RandomStudent();
+		Console.WriteLine(rds.Next());
+	}
+}
